2b4db830ad1243999cba68d59d5b7c5701ea8900
Borrabeer <borrabeer@outlook.com>
2021-04-28 18:25:22 +0700

    Update Mongo Address

--- backend/src/mongoose-connect.js
+++ backend/src/mongoose-connect.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
+import fs from "fs";
+const ca = [fs.readFileSync("./rds-combined-ca-bundle.pem")];
 
 mongoose.Promise = Promise;
 mongoose.connect(
-  `mongodb://docdb-2021-04-28-10-33-17.cluster-co08z1ygs8vo.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`,
+  `mongodb://borrabeer:${process.env.DB_PASSWORD}@docdb-2021-04-28-10-33-17.cluster-co08z1ygs8vo.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`,
   {
-    dbName: "ecommerce",
-    user: "borrabeer",
-    pass: process.env.DB_PASSWORD,
     promiseLibrary: Promise,
     useNewUrlParser: true,
+    sslValidate: true,
+    sslCA: ca,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
