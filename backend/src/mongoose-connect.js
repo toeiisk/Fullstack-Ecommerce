6d2055cb6cae2ecfe5a187291709a005fb17c516
Borrabeer <borrabeer@outlook.com>
2021-04-28 17:58:03 +0700

    Update Mongo Address

--- backend/src/mongoose-connect.js
+++ backend/src/mongoose-connect.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
 mongoose.Promise = Promise;
-mongoose.connect(`mongodb+srv://cluster0.8xcag.mongodb.net`, {
-  dbName: "ecommerce",
-  user: "borrabeer",
-  pass: process.env.DB_PASSWORD,
-  promiseLibrary: Promise,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  `mongodb://docdb-2021-04-28-10-33-17.cluster-co08z1ygs8vo.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`,
+  {
+    dbName: "ecommerce",
+    user: "borrabeer",
+    pass: process.env.DB_PASSWORD,
+    promiseLibrary: Promise,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
